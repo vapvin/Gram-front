833f642e8ec7a64a4584385b76e26c42cf53b307
vapvin <tpehdj12@daum.net>
2020-02-13 19:34:00 +0900

    data is undefined

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -72,7 +72,8 @@ const ME = gql`
 
 export default withRouter(({ history }) => {
   const search = useInput('');
-  const { data } = useQuery(ME);
+  const { data = '' } = useQuery(ME);
+  console.log(data);
   const onSearchSubmit = e => {
     e.preventDefault();
     history.push(`/search?term=${search.value}`);
